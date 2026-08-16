3ed353714d2047dc85ef3f37e5ad0aea9af33b9c
carlos8418 <carlospinheiroeduardosilva@gmail.com>
2026-08-16 00:02:39 -0400

    Update obrigacoes.py

--- obrigacoes.py
+++ obrigacoes.py
@@ -1,32 +1,18 @@
-import datetime
 import requests
-import zoneinfo
 
 NOME_DO_SEU_CANAL = "minha_obrigacoes_Carlos_Guaxe"
 
 def enviar_notificacao(mensagem):
     url = f"https://ntfy.sh/{NOME_DO_SEU_CANAL}"
     try:
-        requests.post(
+        response = requests.post(
             url,
             data=mensagem.encode('utf-8'),
             headers={"Title": "Alerta de Tarefa!"}
         )
+        print(f"Status do envio: {response.status_code}")
     except Exception as e:
         print(f"Erro: {e}")
 
-def verificar_obrigacoes():
-    # Obtém a data exata no fuso horário do Brasil (São Paulo)
-    fuso_br = zoneinfo.ZoneInfo("America/Sao_Paulo")
-    hoje = datetime.datetime.now(fuso_br).date()
-    dia_semana = hoje.weekday()  # 0=Segunda, 1=Terça, 2=Quarta, 3=Quinta, 4=Sexta, 5=Sábado, 6=Domingo
-
-    # --- SUAS OBRIGAÇÕES SEMANAIS ---
-    if dia_semana == 0:
-        enviar_notificacao("Hoje é dia de: Verificar diário de obras")
-    
-    elif dia_semana == 3:
-        enviar_notificacao("Hoje é dia de: Preencher planilhas de terceiros")
-
 if __name__ == "__main__":
-    verificar_obrigacoes()
+    enviar_notificacao("Teste Ntfy: O robô na nuvem está funcionando!")
