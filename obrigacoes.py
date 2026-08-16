5d28cc587436aa3d029f2031ff069105fde2fc21
carlos8418 <carlospinheiroeduardosilva@gmail.com>
2026-08-15 23:48:42 -0400

    Update obrigacoes.py

--- obrigacoes.py
+++ obrigacoes.py
@@ -1,7 +1,7 @@
 import datetime
 import requests
 
-NOME_DO_SEU_CANAL = "minha_obrigacoes_Carlos_Guaxe"
+NOME_DO_SEU_CANAL = "minhas_obrigacoes_Carlos_Guaxe"
 
 def enviar_notificacao(mensagem):
     url = f"https://ntfy.sh/{NOME_DO_SEU_CANAL}"
@@ -18,22 +18,12 @@ def verificar_obrigacoes():
     hoje = datetime.date.today()
     dia_semana = hoje.weekday()  # 0=Segunda, 1=Terça, 2=Quarta, 3=Quinta, 4=Sexta, 5=Sábado, 6=Domingo
 
-    # --- TESTE PARA HOJE ---
+    # --- TESTE PARA HOJE (SÁBADO) ---
     if dia_semana == 5:
         enviar_notificacao("Teste Ntfy: O robô na nuvem está funcionando!")
 
     # --- SUAS OBRIGAÇÕES SEMANAIS ---
     elif dia_semana == 0:
-        enviar_notificacao("Hoje é dia de: Verificar diário de obras")
-    
-    elif dia_semana == 3:
-        enviar_notificacao("Hoje é dia de: Preencher planilhas de terceiros")
-
-if __name__ == "__main__":
-    verificar_obrigacoes()
-
-    # --- SUAS OBRIGAÇÕES SEMANAIS ---
-    if dia_semana == 0:
         enviar_notificacao("Hoje é dia de: Verificar diário de obras")
     
     elif dia_semana == 3:
