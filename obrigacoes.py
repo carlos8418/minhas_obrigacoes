abd7984058ad3257dba5b31c1864231199e25d01
carlos8418 <carlospinheiroeduardosilva@gmail.com>
2026-09-14 16:36:47 -0400

    Update obrigacoes.py

--- obrigacoes.py
+++ obrigacoes.py
@@ -23,19 +23,20 @@ def verificar_obrigacoes():
 
     # --- SUAS OBRIGAÇÕES SEMANAIS ---
     if dia_semana == 0:
-        enviar_notificacao("Hoje é dia de: Verificar diário de obras")
+        enviar_notificacao("Hoje é dia de: Verificar diário de obras"; "HOJE É DIA DE PREENCHER PANILHA DE TERCEIROS")
     elif dia_semana == 1:
-        enviar_notificação("Hoje é dia de: Extrair as planilhas de combustivel/terceiros e corrigi-las no excel")
+        enviar_notificação("Hoje é dia de:Extrair as planilhas de insumos e corrigi-la no excel ")
         
     elif dia_semana == 2:
-        enviar_notificacao("Hoje é dia de:Extrair as planilhas de insumos e corrigi-la no execel")
+        enviar_notificacao("Hoje é dia de:PREENCHER PANILHA DE MÃO DE OBRA")
     
     elif dia_semana == 3:
-        enviar_notificacao("Hoje é dia de: Preencher planilhas de terceiros")
+        enviar_notificacao("Hoje é dia de: Preencher planilhas de terceiros"; "HOJE É DIA DE PREENCHER PANILHA DE EQUIPAMENTO")
+    
     # lembrete semanal
     # Dispara no dia 2 do mes (3 dias antes do dia 5)
     if hoje.day == 2:
-        enviar_notificação("ATENÇÃO: Faltam 3 dias par o prazo final (dia 05) de enviar a planilha de insumos!")
+        enviar_notificação("ATENÇÃO: Faltam 3 dias par o prazo final (dia 05) de enviar a MEDIÇÃO!")
 
 if __name__ == "__main__":
     verificar_obrigacoes()
